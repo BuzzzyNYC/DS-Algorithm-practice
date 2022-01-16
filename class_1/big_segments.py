d982dc5aaac4f07345e48fcebdea7f60d2ae48e3
BuzzzyNYC <nguyentranglilo9192@gmail.com>
2022-01-16 18:35:29 -0500

    Updated file

--- class_1/big_segments.py
+++ class_1/big_segments.py
@@ -1,9 +1,24 @@
+'''
+problem analysis:
+- n segments with a coordinate [l,r]
+- find a segment that covers all other segments --> print the order of the segment
+- else print -1
+
+'''
+
+from cmath import inf
+
+
 n = int(input())
-# input n times the segment
-for 
-l = list(map(int, input().split()))
-r = list(map(int, input().split()))
-min_l = 0
-max_r = 0
-for i in range(2, n):
-    if 
+minl = float(inf)
+maxr = 0
+number = 0
+for i in range(1, n + 1):
+    l, r = map(int, input().split())
+    if l <= minl and r >= maxr:
+        number = i
+    elif l < minl or r > maxr:
+        number = -1
+    minl = min(minl, l)
+    maxr = max(maxr, r)
+print(number)
